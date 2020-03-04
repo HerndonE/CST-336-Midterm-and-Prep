52435cef1ce473834f1e2a452176417580eadebf
HerndonE <EHerndon17@gmail.com>
2020-03-04 15:44:46 -0800

    midterm upload

---
+++ Midterm/js/functions.js
@@ -0,0 +1,34 @@
+
+
+//0451526538
+//1594200092
+function buildBook() 
+{
+    
+    $.ajax({
+        type: "GET",
+        url: "https://openlibrary.org/api/books?bibkeys=ISBN:"+$('#number-books').val()+"&format=json&jscmd=data",
+        dataType: "json",
+        data: { 
+        },
+        success: function(result, status) 
+        {
+           console.log(result);
+         
+            $('#results5').html("<img src="+result["ISBN:"+$('#number-books').val()].cover.large+">");
+            $("#results").html("Title: " + result["ISBN:"+$('#number-books').val()].title);
+            $("#results1").html("Author: " + result["ISBN:"+$('#number-books').val()].authors.name);
+            $("#results2").html("Publisher: " + result["ISBN:"+$('#number-books').val()].publish_date);
+            $("#results3").html("ISBN: " + result["ISBN:"+$('#number-books').val()]);
+            $("#results4").html("Pages: " + result["ISBN:"+$('#number-books').val()].number_of_pages);
+          
+           
+        }
+    }); //ajax 
+
+}
+
+
+
+
+
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,5 +1,34 @@
 Name: Ethan Herndon
 OtterID: hern3998
 
-
 Answers for Part 1
+
+Question 1
+a. List all possible ways to apply a CSS style into an HTML page and what are their
+precedence order?
+All the possible ways to apply CSS style into an HTML page is inline,
+embedded/internal,linked/external, and imported.
+The precedence order is inline Style > ID > Class Style > Element tag
+b. What is the difference between "margin' and "padding" in Box Model
+The padding clears an area around the content and it is transparent.
+A margin however clears an area outside the border and its transparent like padding.
+	
+Question 2
+a. What is the difference between "!==" and "!===" in JavaScript Language?
+"!==" means not equal value and type. "!==="
+b. What is the value of the variable statement?
+false
+
+Question 3
+[1,2]
+[1,2]
+
+Question 4
+Draw the DOM structure of the following html page
+a. <HTML>
+	i. <head>
+		1.<title>
+			a. My page
+	ii. <body>
+		1. <img>
+		2. <p>
